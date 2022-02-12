4afd336c6dfb834bf15dadf94a18187095968b36
msaaddev <mrsaadirfan@gmail.com>
2022-02-12 23:45:30 +0500

    📦 NEW: Dashboard page

---
+++ pages/dashboard.js
@@ -0,0 +1,12 @@
+import Nav from 'components/common/Nav';
+import css from 'styles/Dashboard.module.css';
+
+const Dashboard = () => {
+	return (
+		<div className={css.container}>
+			<Nav />
+		</div>
+	);
+};
+
+export default Dashboard;
