bd34afa492c83bc6ace3db0625426270e9730a0c
msaaddev <mrsaadirfan@gmail.com>
2022-02-12 23:45:45 +0500

    📦 NEW: Added Nav component

---
+++ components/common/Nav.js
@@ -0,0 +1,23 @@
+import css from 'styles/Nav.module.css';
+import Link from 'next/link';
+
+const Nav = () => {
+	return (
+		<div className={css.container}>
+			<div className={css.logo}>
+				<img src="/polling.png" alt="logo" />
+				<h2>Pollify</h2>
+			</div>
+			<div className={css.options}>
+				<Link href="/create">
+					<a className={css.option}>Create Poll</a>
+				</Link>
+				<Link href="/signup">
+					<a className={css.option}>Logout</a>
+				</Link>
+			</div>
+		</div>
+	);
+};
+
+export default Nav;
